a28b5b5145c453abe73dba1414cc5c442fca463c
Walter Schreppers <walter@sitweb.eu>
2019-06-22 16:33:14 +0200

    Mac autostart plist and install script (will be moved into gui/settings to activate/deactivate autostart feature).

---
+++ mac_autostart_install/install.sh
@@ -0,0 +1,27 @@
+make
+
+# already copied using make gui in anykey_save_tool folder now!
+#cp ~/bin/anykey_save AnyKey.app/Contents/MacOS/
+#cp ~/bin/anykey_crd AnyKey.app/Contents/MacOS/
+
+echo "remove old version..."
+rm -rf /Applications/AnyKey.app
+
+echo "copy new version..."
+cp -r AnyKey.app /Applications/
+#also compress it ;)
+# brew install upx
+
+echo "compress it..."
+upx -9 /Applications/AnyKey.app/Contents/MacOS/AnyKey
+upx -9 /Applications/AnyKey.app/Contents/MacOS/anykey_crd
+upx -9 /Applications/AnyKey.app/Contents/MacOS/anykey_save
+
+echo "update launchctl daemon and copy plist file..."
+cp org.anykey.configurator.plist ~/Library/LaunchAgents/org.anykey.configurator.plist
+launchctl unload ~/Library/LaunchAgents/org.anykey.configurator.plist
+launchctl load ~/Library/LaunchAgents/org.anykey.configurator.plist
+
+echo "done"
+
+
