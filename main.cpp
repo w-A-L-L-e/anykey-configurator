3739266201459daa4dde3fcb9a6bb2e57bebbf0f
Walter Schreppers <walter@sitweb.eu>
2019-06-25 01:25:38 +0200

    added scaling for windows with retina or 4k display

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[])
 {
+    // make windows with retina or 4k still look good
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
