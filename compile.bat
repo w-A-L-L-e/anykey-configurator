f13e3cc07a5690aafdd71ce606e59f01e773b418
w-A-L-L-e <walter@sitweb.eu>
2019-09-28 23:26:43 +0100

    New release built and some bugfixes in the package and compile bat scripts for windows.

--- compile.bat
+++ compile.bat
@@ -1,23 +1,25 @@
-;=============================================================================
-;  author        : Walter Schreppers
-;  filename      : compile.bat
-;  created       : 1/3/2018
-;  modified      :
-;  version       :
-;  copyright     : Walter Schreppers
-;  bugreport(log):
-;  description   : Compile batch file for MinGW windows 10
-;=============================================================================
+:: =============================================================================
+::  author        : Walter Schreppers
+::  filename      : compile.bat
+::  created       : 1/3/2018
+::  modified      :
+::  version       :
+::  copyright     : Walter Schreppers
+::  bugreport(log):
+::  description   : Compile batch file for MinGW windows 10
+::=============================================================================
 
-
-echo "Setting path to use mingw of Qt"
+@echo off
+cls
+echo Setting path to use mingw of Qt
 set PATH=C:\WinAVR-20100110\bin;C:\WinAVR-20100110\utils\bin;C:\Program Files (x86)\Intel\iCLS Client\;C:\Program Files\Intel\iCLS Client\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\Git\cmd;C:\Qt\Qt5.6.3\Tools\mingw492_32\bin;C:\Qt\Static\5.6.3\bin;C:\Users\Walter Schreppers\AppData\Local\Microsoft\WindowsApps
 
+echo copying .pro_windows to .pro file
 copy AnyKey.pro_windows AnyKey.pro
 
-echo "make clean, and run qmake"
+echo make clean, and run qmake
 qmake AnyKey.pro_windows
 make -f Makefile.release clean
 
-echo "compile again with make"
+echo compile release configurator...
 make -f Makefile.release
--- package.bat
+++ package.bat
@@ -9,15 +9,22 @@
 ;  description   : Generate windows 10 installer and compress binaries
 ;=============================================================================
 
-echo "compressing exe for distribution..."
+@echo off
+cls
+
+echo copy latest save and crd tools from anykey_save_tool 
+copy ..\anykey_save_tool\anykey_save.exe release\anykey_save.exe
+copy ..\anykey_save_tool\anykey_crd.exe release\anykey_crd.exe
+
+echo Compressing executables for distribution...
 windows-installer\upx.exe release\AnyKey.exe
 windows-installer\upx.exe release\anykey_save.exe
 windows-installer\upx.exe release\anykey_crd.exe
 
-echo "release\AnyKey.exe built successfull generating new installer..."
+echo Making installer for release\AnyKey.exe ...
 cd windows-installer
 "c:\Program Files (x86)\NSIS\Bin\makensis.exe" anykey_installer.nsi
 cd ..
 
-echo "Configurator installer saved in windows-installer dir"
+echo Configurator installer saved in 'windows-installer' dir
 
