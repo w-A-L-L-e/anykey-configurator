17c12f7d99ce16bfb6f542d4d8f6c00b56131515
Walter Schreppers <walter@sitweb.eu>
2020-06-14 18:21:54 +0200

    linux compile helper script

---
+++ linux_compile.sh
@@ -0,0 +1,26 @@
+#=============================================================================
+# author        : Walter Schreppers
+# filename      : mac_install.sh
+# created       : 14/6/2020
+# modified      :
+# version       :
+# copyright     : Walter Schreppers
+# bugreport(log):
+# description   : Compile and package helper script for Linux
+#=============================================================================
+
+cp AnyKey.pro_linux AnyKey.pro
+
+echo "remove old version..."
+rm AnyKey
+
+qmake AnyKey.pro
+make
+
+echo "todo copy anykey_save and crd builds here"
+# already copied using make gui in anykey_save_tool folder now!
+#cp ~/bin/anykey_save AnyKey.app/Contents/MacOS/
+#cp ~/bin/anykey_crd AnyKey.app/Contents/MacOS/
+
+echo "done"
+
